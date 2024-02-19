13aee0cddeb79ae9a2ba303e780578eba4a7d931
Josep Boix Requesens <jspboix@gmail.com>
2024-02-19 16:38:44 +0100

    feat(showcase): add link to github in code examples
    Enhances the code examples in the showcase by implementing the following changes:
    
    - Modified the styling to the code block to make it more readable.
    - Added light and dark modes for code highlighting.
    - Added a footer to the code block with a direct link to the corresponding file on GitHub.

--- src/layout/content/showcase/showcase-component.js
+++ src/layout/content/showcase/showcase-component.js
@@ -2,13 +2,15 @@ import { html, LitElement, unsafeCSS } from 'lit';
 import '../../../components/code-block/code-block';
 import { theme } from '../../../theme/theme';
 import showcaseComponentCss from './showcase-component.scss?inline';
+import { when } from 'lit/directives/when.js';
 
 export class ShowcaseComponent extends LitElement {
   static styles = [theme, unsafeCSS(showcaseComponentCss)];
 
   static properties = {
     hasCodeExample: { type: Boolean, state: true },
-    exampleLanguage: { type: String }
+    exampleLanguage: { type: String },
+    href: { type: String }
   };
 
   connectedCallback() {
@@ -29,7 +31,20 @@ export class ShowcaseComponent extends LitElement {
 
       <div class="${!this.hasCodeExample ? 'hidden' : ''}">
         <h3>Implementation</h3>
-        <slot name="code"></slot>
+        <div part="implementation">
+          <div part="implementation-code">
+            <slot name="code"></slot>
+          </div>
+          ${when(this.href, () => html`
+            <div part="implementation-footer">
+              <a href="https://github.com/SRGSSR/pillarbox-web-demo/blob/main/static/showcases/${this.href}"
+                 part="implementation-link"
+                 target="_blank">
+                view on github
+              </a>
+            </div>
+          `)}
+        </div>
       </div>
     `;
   }
