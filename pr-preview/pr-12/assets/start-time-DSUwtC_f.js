25e0e9c1f4c5e3c84d205de78c8e914d432a2a9d
jboix <jboix@users.noreply.github.com>
2024-02-20 15:57:18 +0000

    Remove preview for PR 12 🛬

--- pr-preview/pr-12/assets/start-time-DSUwtC_f.js
+++
@@ -1,2 +0,0 @@
-import{p as r}from"./pillarbox.es-B5lSq2Wr.js";/* empty css                        */const e=r("video-element-id",{fill:!0});e.src({src:"urn:rts:video:6820736",type:"srgssr/urn"});e.on("loadeddata",()=>{e.currentTime(300)});document.querySelector("#close-btn").addEventListener("click",()=>{window.close()});window.pillarbox=r;
-//# sourceMappingURL=start-time-DSUwtC_f.js.map
