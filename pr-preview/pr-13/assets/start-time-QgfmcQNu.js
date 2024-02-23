b93dee12df562f20061211bcd61725402405f771
jboix <jboix@users.noreply.github.com>
2024-02-23 20:23:02 +0000

    Remove preview for PR 13 🛬

--- pr-preview/pr-13/assets/start-time-QgfmcQNu.js
+++
@@ -1,2 +0,0 @@
-import{p as r}from"./core-demo-header-component-DLPN1d3Z.js";/* empty css                        */const e=r("video-element-id",{fill:!0});e.src({src:"urn:rts:video:6820736",type:"srgssr/urn"});e.on("loadeddata",()=>{e.currentTime(300)});document.querySelector("#close-btn").addEventListener("click",()=>{window.close()});window.pillarbox=r;
-//# sourceMappingURL=start-time-QgfmcQNu.js.map
