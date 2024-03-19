99d75e9feaae5dbb817d6e417127ebdbbe81b17f
jboix <jboix@users.noreply.github.com>
2024-03-19 10:21:24 +0000

    Deploy preview for PR 25 🛫

---
+++ pr-preview/pr-25/assets/start-time-B3NxXFDC.js
@@ -0,0 +1,2 @@
+import{p as r}from"./core-demo-header-component-RoiX7w6o.js";/* empty css                        */const e=r("video-element-id",{fill:!0});e.src({src:"urn:rts:video:6820736",type:"srgssr/urn"});e.on("loadeddata",()=>{e.currentTime(300)});document.querySelector("#close-btn").addEventListener("click",()=>{window.close()});window.pillarbox=r;
+//# sourceMappingURL=start-time-B3NxXFDC.js.map
