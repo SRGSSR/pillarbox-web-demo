63cbde0d5d995112502110517ca29c0da61c460e
Josep Boix Requesens <jspboix@gmail.com>
2024-03-20 10:44:20 +0100

    chore(samples): add vue sample
    Resolves #24 by introducing a Vue sample for easy integration of pillarbox as a
    component, providing a starting point for integrators.

---
+++ samples/vue/src/components/Pillarbox.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { onMounted } from 'vue';
+import pillarbox from '@srgssr/pillarbox-web';
+import '@srgssr/pillarbox-web/dist/pillarbox.min.css';
+
+// Define props
+const props = defineProps({
+  src: String,
+  type: {
+    type: String,
+    default: 'srgssr/urn',
+  },
+});
+
+onMounted(() => {
+  const player = pillarbox('main-player');
+  player.src({ src: props.src, type: props.type });
+});
+</script>
+
+<template>
+  <video id="main-player" class="video-js pillarbox-js" controls crossOrigin="anonymous"></video>
+</template>
+
+<style scoped>
+@import "@srgssr/pillarbox-web";
+</style>
