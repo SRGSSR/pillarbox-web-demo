ea830a05d1fd67e2418844b4f4ba76c0d103bbca
jboix <jboix@users.noreply.github.com>
2025-07-10 13:33:04 +0000

    Deploy preview for PR 72 🛫

---
+++ pr-preview/pr-72/assets/playback-rate-BmHZ1rSa.js
@@ -0,0 +1,2 @@
+import{p as e}from"./core-demo-header-component-CKumsNp2.js";const r=e("video-element-id",{fill:!0,muted:!0,playbackRates:[.5,.75,1,1.25,1.5,2]});r.src({src:"urn:rts:video:9883196",type:"srgssr/urn"});window.player=r;document.querySelector("#close-btn").addEventListener("click",()=>{window.close()});window.pillarbox=e;
+//# sourceMappingURL=playback-rate-BmHZ1rSa.js.map
