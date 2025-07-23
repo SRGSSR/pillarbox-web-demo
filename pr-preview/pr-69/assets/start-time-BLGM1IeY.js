97bdb306795724619b42e9c4e6692a3dc6e5b91a
jboix <jboix@users.noreply.github.com>
2025-07-23 13:32:44 +0000

    Deploy preview for PR 69 🛫

--- pr-preview/pr-69/assets/start-time-BLGM1IeY.js
+++
@@ -1,2 +0,0 @@
-import{p as r}from"./core-demo-header-component-Cvya-StE.js";/* empty css                        */const e=r("video-element-id",{muted:!0});e.src({src:"urn:rts:video:6820736",type:"srgssr/urn"});e.on("loadeddata",()=>{e.currentTime(300)});document.querySelector("#close-btn").addEventListener("click",()=>{window.close()});window.pillarbox=r;
-//# sourceMappingURL=start-time-BLGM1IeY.js.map
---
+++ pr-preview/pr-69/assets/start-time-C0awLERb.js
@@ -0,0 +1,2 @@
+import{p as r}from"./core-demo-header-component-CXdk6c17.js";/* empty css                        */const e=r("video-element-id",{muted:!0});e.src({src:"urn:rts:video:6820736",type:"srgssr/urn"});e.on("loadeddata",()=>{e.currentTime(300)});document.querySelector("#close-btn").addEventListener("click",()=>{window.close()});window.pillarbox=r;
+//# sourceMappingURL=start-time-C0awLERb.js.map
