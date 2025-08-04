6c84f49b0e603a81d327f3725c0f6eac15d85319
Josep Boix Requesens <jspboix@gmail.com>
2025-08-04 14:36:14 +0200

    chore: update samples dependencies
    Update all sample to use the latest pillarbox version as well as the latest verison
    of each sampled framework:
    
    - Updated React from version 18 to 19.
    - Updated Vite from version 5 to 7.
    - Updated Angular from version 18 to 20.

---
+++ samples/angular/src/shims/pillarbox.d.ts
@@ -0,0 +1,5 @@
+declare module '@srgssr/pillarbox-web' {
+  export {
+    Pillarbox as default, DataProvider, MediaComposition, PillarboxMonitoring, Player, SRGAnalytics, SrgSsr
+  } from '@srgssr/pillarbox-web/dist/types/build';
+}
