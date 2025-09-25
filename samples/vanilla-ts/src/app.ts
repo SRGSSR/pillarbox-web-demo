91c0ecbc42b499d63277f179f854c0bf96fa954f
Josep Boix Requesens <jspboix@gmail.com>
2025-09-25 09:45:59 +0200

    chore(samples): update pillarbox version for typescript samples
    Update Pillarbox to the latest version which fixes the `exports.types` field in
    `package.json`, ensuring the correct TypeScript entry point is resolved.
    
    This update refreshes the `vanilla-ts` and `angular` samples to use the fixed
    typings and verify proper type support and the latest versions of other
    dependencies.

--- samples/vanilla-ts/src/app.ts
+++ samples/vanilla-ts/src/app.ts
@@ -1,4 +1,4 @@
-import {default as pillarbox, Player as PillarboxPlayer} from "@srgssr/pillarbox-web";
+import pillarbox, {Player} from "@srgssr/pillarbox-web";
 
-const player = pillarbox('main-player') as PillarboxPlayer;
+const player = pillarbox('main-player') as Player;
 player.src({src: 'urn:rts:video:13774277', type: 'srgssr/urn'});
