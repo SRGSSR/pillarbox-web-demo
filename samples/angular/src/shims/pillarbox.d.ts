3cd077efef0585cfbab538681ee7515c011f3442
Josep Boix Requesens <jspboix@gmail.com>
2025-09-25 09:45:59 +0200

    chore(samples): update pillarbox version for typescript samples
    Update Pillarbox to the latest version which fixes the `exports.types` field in
    `package.json`, ensuring the correct TypeScript entry point is resolved.
    
    This update refreshes the `vanilla-ts` and `angular` samples to use the fixed
    typings and verify proper type support and the latest versions of other
    dependencies.

--- samples/angular/src/shims/pillarbox.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '@srgssr/pillarbox-web' {
-  export {
-    Pillarbox as default, DataProvider, MediaComposition, PillarboxMonitoring, Player, SRGAnalytics, SrgSsr
-  } from '@srgssr/pillarbox-web/dist/types/build';
-}
