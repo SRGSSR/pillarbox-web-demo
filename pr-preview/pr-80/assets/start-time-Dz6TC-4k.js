d0f25c6a8e480658c461014125d74b7241970a97
amtins <amtins@users.noreply.github.com>
2025-10-01 13:54:43 +0000

    Remove preview for PR 80 🛬

--- pr-preview/pr-80/assets/start-time-Dz6TC-4k.js
+++
@@ -1,2 +0,0 @@
-import{p as r}from"./core-demo-header-component-CwvlAg54.js";/* empty css                        */const e=r("video-element-id",{muted:!0});e.src({src:"urn:rts:video:6820736",type:"srgssr/urn"});e.on("loadeddata",()=>{e.currentTime(300)});document.querySelector("#close-btn").addEventListener("click",()=>{window.close()});window.pillarbox=r;
-//# sourceMappingURL=start-time-Dz6TC-4k.js.map
