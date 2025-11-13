3433d4e2f330973613ec1d58220e1f6ea94e5849
Josep Boix Requesens <jspboix@gmail.com>
2025-11-13 15:38:28 +0100

    fix: angular pillarbox component
    The pillarbox component in the remote repository is not the intended for the angular sample,
    it was mistakenly changed to that of another app for debugging a particular bug.
    
    This commit restores the original pillarbox componetn from the sample.

--- samples/angular/src/app/pillarbox.component.ts
+++ samples/angular/src/app/pillarbox.component.ts
@@ -1,70 +1,30 @@
-import { ElementRef, Injectable } from "@angular/core";
-import pillarbox, { Player } from "@srgssr/pillarbox-web";
+import {AfterViewInit, Component, Input, OnChanges, SimpleChanges} from "@angular/core";
+import {default as pillarbox, Player as PillarboxPlayer} from "@srgssr/pillarbox-web";
 
-@Injectable({ providedIn: "root" })
-export class PillarboxComponent {
-  private readonly players = new Map<string, Player>();
+@Component({
+  selector: 'pb-player',
+  imports: [],
+  template: `
+    <video id="main-player" class="video-js pillarbox-js" controls crossOrigin="anonymous"></video>
+  `
+})
+export class PillarboxComponent implements AfterViewInit, OnChanges {
+  @Input() type: string = 'srgssr/urn';
+  @Input() src!: string;
 
-  dispose(playerId: string): void {
-    this.disposePlayer(playerId);
+  private player?: PillarboxPlayer;
+  ngAfterViewInit(): void {
+    this.player = pillarbox('main-player') as PillarboxPlayer;
+    this.updatePlayerSource();
   }
-
-  disposePlayer(playerId: string): void {
-    this.players.get(playerId)?.dispose();
-    this.players.delete(playerId);
-  }
-
-  hasPlayer(playerId: string): boolean {
-    return this.players.has(playerId);
-  }
-
-  initPlayer(playerId: string, container: ElementRef, audioOnlyMode: boolean): void {
-    const options = {
-      audioOnlyMode,
-      playbackRates: [0.5, 1, 1.5, 2],
-      fluid: true,
-      titleBar: false,
-    };
-    const player: Player = pillarbox(container.nativeElement, options) as Player;
-    this.players.set(playerId, player);
-  }
-
-  initOrSetSource(playerId: string, source: string, container?: ElementRef, audioOnlyMode?: boolean): void {
-    if (!this.hasPlayer(playerId) && container && audioOnlyMode !== undefined) {
-      this.initPlayer(playerId, container, audioOnlyMode);
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['src'] || changes['type']) {
+      this.updatePlayerSource();
     }
-    this.setSource(playerId, source);
   }
-
-  getCurrentTime(playerId: string): number {
-    return this.players.get(playerId)?.currentTime() ?? 0;
-  }
-
-  setCurrentTime(playerId: string, value: number): void {
-    this.players.get(playerId)?.currentTime(value);
-  }
-
-  hidePoster(playerId: string): void {
-    this.players.get(playerId)?.addClass("vjs-has-started");
-  }
-
-  paused(playerId: string): boolean {
-    return this.players.get(playerId)?.paused() ?? true;
-  }
-
-  pause(playerId: string): void {
-    this.players.get(playerId)?.pause();
-  }
-
-  async play(playerId: string): Promise<void> {
-    await this.players.get(playerId)?.play();
-  }
-
-  setSource(playerId: string, source: string): void {
-    if (source.includes("urn:")) {
-      this.players.get(playerId)?.src({ src: source, type: "srgssr/urn" });
-    } else {
-      this.players.get(playerId)?.src(source);
+  private updatePlayerSource(): void {
+    if (this.player && this.src && this.type) {
+      this.player.src({src: this.src, type: this.type});
     }
   }
 }
