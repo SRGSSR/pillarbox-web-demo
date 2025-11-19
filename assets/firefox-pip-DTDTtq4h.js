8193f1d6a2352c15b8d9039a7cec11d55d1be09c
jboix <jspboix@gmail.com>
2025-11-19 12:47:24 +0000

    Deploying to gh-pages from @ SRGSSR/pillarbox-web-demo@05da969bc43161703d88851a0e338cf9d143a8fd 🚀

--- assets/firefox-pip-DTDTtq4h.js
+++
@@ -1,2 +0,0 @@
-import{p as e}from"./core-demo-header-component-D2o7ekP1.js";const r=e("video-element-id",{fill:!0,muted:!0,disablePictureInPicture:e.browser.IS_FIREFOX});r.src({src:"urn:rts:video:9883196",type:"srgssr/urn"});window.player=r;document.querySelector("#close-btn").addEventListener("click",()=>{window.close()});window.pillarbox=e;
-//# sourceMappingURL=firefox-pip-DTDTtq4h.js.map
