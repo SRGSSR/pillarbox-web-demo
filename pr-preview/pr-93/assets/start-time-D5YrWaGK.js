9c7a25bb88baff2dc5eb065ca6f9a20bd9ba5f15
jboix <jboix@users.noreply.github.com>
2026-02-02 13:11:24 +0000

    Deploy preview for PR 93 🛫

---
+++ pr-preview/pr-93/assets/start-time-D5YrWaGK.js
@@ -0,0 +1,2 @@
+import{p as r}from"./core-demo-header-component-CiGH9v1l.js";/* empty css                        */const e=r("video-element-id",{muted:!0});e.src({src:"urn:rts:video:6820736",type:"srgssr/urn"});e.on("loadeddata",()=>{e.currentTime(300)});document.querySelector("#close-btn").addEventListener("click",()=>{window.close()});window.pillarbox=r;
+//# sourceMappingURL=start-time-D5YrWaGK.js.map
