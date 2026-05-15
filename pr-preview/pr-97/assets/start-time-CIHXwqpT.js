a61433ac7f11e30763da28bb9485a718f65ec8ee
jboix <jboix@users.noreply.github.com>
2026-05-15 18:03:37 +0000

    Remove preview for PR 97 🛬

--- pr-preview/pr-97/assets/start-time-CIHXwqpT.js
+++
@@ -1,2 +0,0 @@
-import{p as r}from"./core-demo-header-component-CWQBvAYW.js";/* empty css                        */const e=r("video-element-id",{muted:!0});e.src({src:"urn:rts:video:6820736",type:"srgssr/urn"});e.on("loadeddata",()=>{e.currentTime(300)});document.querySelector("#close-btn").addEventListener("click",()=>{window.close()});window.pillarbox=r;
-//# sourceMappingURL=start-time-CIHXwqpT.js.map
