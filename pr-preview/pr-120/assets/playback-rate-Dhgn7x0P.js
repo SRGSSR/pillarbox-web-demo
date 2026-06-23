eda209338c189ce3fd482f179e91ccdb4a7ecb1f
jboix <jboix@users.noreply.github.com>
2026-06-23 14:57:14 +0000

    Remove preview for PR 120 🛬

--- pr-preview/pr-120/assets/playback-rate-Dhgn7x0P.js
+++
@@ -1,2 +0,0 @@
-import{o as e}from"./core-demo-header-component-C6PDx-DJ.js";var t=e(`video-element-id`,{fill:!0,muted:!0,playbackRates:[.5,.75,1,1.25,1.5,2]});t.src({src:`urn:rts:video:9883196`,type:`srgssr/urn`}),window.player=t,document.querySelector(`#close-btn`).addEventListener(`click`,()=>{window.close()}),window.pillarbox=e;
-//# sourceMappingURL=playback-rate-Dhgn7x0P.js.map
